c09e6442611c98bb7769c11c832204ec6cde94c5
elenasanchezp <elena.sanchez.p@outlook.com>
2017-03-14 23:14:23 +0000

    Updated SEO metadata. Updated NottsJS logo.

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,13 +3,13 @@ require('styles/App.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+let nottsJSlogoImage = require('../images/NottsJS.png');
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
+        <img src={nottsJSlogoImage} alt="NottsJS" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
       </div>
     );
