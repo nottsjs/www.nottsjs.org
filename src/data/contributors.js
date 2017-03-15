cdc763c8dd60bee8f93c8914e1b9bba513a1d8a5
Chris Kemp <chris.kemp3000@gmail.com>
2017-03-15 20:45:02 +0000

    Added Elena to contributors list

--- src/data/contributors.js
+++ src/data/contributors.js
@@ -1,4 +1,5 @@
 export default [
   { user_id: 'narayanvarma', name: 'Narayan Varma', count: 1 },
-  { user_id: 'leewalkergm', name: 'Lee Walker', count: 1 }
+  { user_id: 'leewalkergm', name: 'Lee Walker', count: 1 },
+  { user_id: 'elenasanchezp', name: 'Elena Sanchez', count: 1 }
 ];
