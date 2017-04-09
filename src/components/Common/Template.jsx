e367c18923e180ad0416ee01c4cedddc844dc40c
Chris Kemp <chris.kemp3000@gmail.com>
2017-04-09 20:15:34 +0100

    Upgraded react-router

--- src/components/Common/Template.jsx
+++ src/components/Common/Template.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 
-import { Layout, Header, Drawer, Content } from 'react-mdl';
+import { Layout, Header, Drawer, Content, Navigation } from 'react-mdl';
+import { Link } from 'react-router-dom';
 
 export default class Template extends React.Component {
   static propTypes = {
     title: React.PropTypes.string.isRequired
+  }
+  renderNav() {
+    return (
+      <Navigation>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/training'>Training</Link>
+      </Navigation>
+    );
   }
   render() {
     const { title } = this.props;
     document.title = title + ' - NottsJS';
     return (
       <Layout fixedHeader>
-        <Header title={title} />
-        <Drawer title={title} />
+        <Header title={title}>
+          {this.renderNav()}
+        </Header>
+        <Drawer title={title}>
+          {this.renderNav()}
+        </Drawer>
         <Content>
           {this.props.children}
         </Content>
--- src/index.jsx
+++ src/index.jsx
@@ -7,17 +7,19 @@ import 'react-mdl/extra/material.js';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 import Home from './components/Pages/Home.jsx';
 import About from './components/Pages/About.jsx';
 import Training from './components/Pages/Training.jsx';
 
 ReactDOM.render(
-    <Router history={hashHistory}>
-        <Route path="/" component={Home}/>
+    <Router>
+      <div>
+        <Route exact path="/" component={Home} />
         <Route path="/About" component={About} />
         <Route path="/Training" component={Training} />
+      </div>
     </Router>,
     document.getElementById('app')
 );
